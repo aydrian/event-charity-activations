8464967737cdcab2efc0ff8ecf6034e54b24116c
Aydrian Howard <aydrian@gmail.com>
2023-04-26 22:24:08 -0400

    added chart

--- app/routes/dashboard.$slug.tsx
+++ app/routes/dashboard.$slug.tsx
@@ -6,7 +6,33 @@ import {
   useLoaderData
 } from "@remix-run/react";
 import QRCode from "qrcode";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 import { prisma } from "~/services/db.server";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip);
+
+function hexToRgbA(hex: string, alpha: number = 1) {
+  var c;
+  if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+    c = hex.substring(1).split("");
+    if (c.length == 3) {
+      c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+    }
+    c = Number("0x" + c.join(""));
+    return `rgba(${[(c >> 16) & 255, (c >> 8) & 255, c & 255].join(
+      ","
+    )},${alpha})`;
+  }
+  throw new Error("Bad Hex");
+}
 
 export const loader = async ({ params }: LoaderArgs) => {
   const { slug } = params;
@@ -33,27 +59,57 @@ export const loader = async ({ params }: LoaderArgs) => {
       status: 404
     });
   }
+  const counts = await prisma.donation.groupBy({
+    by: ["charityId"],
+    where: { eventId: event.id },
+    _count: { charityId: true }
+  });
+  const charities = event.Charities.map((charity) => {
+    return {
+      id: charity.charityId,
+      color: charity.color,
+      donation: charity.donation,
+      name: charity.Charity.name,
+      count:
+        counts.find((item) => item.charityId === charity.charityId)?._count
+          ?.charityId || 0
+    };
+  });
   const qrcode = await QRCode.toDataURL(
     `https://${process.env.VERCEL_URL}/donate/${event.id}`
   );
-  return json({ event, qrcode });
+  return json({ charities, event, qrcode });
 };
 
 export default function EventDashboard() {
-  const { event, qrcode } = useLoaderData<typeof loader>();
+  const { charities, event, qrcode } = useLoaderData<typeof loader>();
   return (
-    <section className="prose mx-auto grid max-w-4xl">
+    <section className="prose mx-auto grid max-w-5xl">
       <h1 className="font-extra-bold mb-0 bg-gradient-to-r from-brand-iridescent-blue to-brand-electric-purple bg-clip-text text-center text-5xl !leading-tight text-transparent sm:text-7xl">
         {event.name}
       </h1>
       <div className="flex justify-items-stretch gap-12">
-        <div className="grow rounded border border-brand-gray-b bg-white p-8 sm:px-16">
-          Chart
-          <ul>
-            {event.Charities.map((charity) => (
-              <li key={charity.charityId}>{charity.Charity.name}</li>
-            ))}
-          </ul>
+        <div className="grow rounded border border-brand-gray-b bg-white p-1">
+          <Bar
+            options={{
+              responsive: true,
+              scales: { y: { ticks: { stepSize: 1 } } }
+            }}
+            data={{
+              labels: charities.map((charity) => charity.name),
+              datasets: [
+                {
+                  label: "total count",
+                  data: charities.map((charity) => charity.count),
+                  backgroundColor: charities.map((charity) =>
+                    hexToRgbA(charity.color, 0.5)
+                  ),
+                  borderColor: charities.map((charity) => charity.color),
+                  borderWidth: 1
+                }
+              ]
+            }}
+          />
           Donations: {event.Donations.length}
         </div>
         <div className="grow rounded border border-brand-gray-b bg-white p-8 sm:px-16">
