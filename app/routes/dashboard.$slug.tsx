89b7ffa059a8e0d561e88c84f1ac7e57ad9287b0
Aydrian Howard <aydrian@gmail.com>
2023-05-10 17:22:46 -0600

    Added twitter to dashboard to fill space on right

--- app/routes/dashboard.$slug.tsx
+++ app/routes/dashboard.$slug.tsx
@@ -152,7 +152,7 @@ export default function EventDashboard() {
               />
             </div>
             <div className="flex shrink flex-col justify-stretch gap-4 text-center">
-              <div className="flex grow flex-col items-center rounded border border-brand-gray-b bg-white p-2">
+              <div className="flex grow flex-col items-center justify-center rounded border border-brand-gray-b bg-white p-2">
                 <div className="flex h-12 w-12 items-center justify-center rounded-full bg-brand-electric-purple p-2">
                   <GiftIcon className="aspect-square h-full text-gray-100" />
                 </div>
@@ -163,7 +163,7 @@ export default function EventDashboard() {
                   Total Donations
                 </div>
               </div>
-              <div className="flex grow flex-col items-center gap-1 rounded border border-brand-gray-b bg-white p-2">
+              <div className="flex grow flex-col items-center justify-center gap-1 rounded border border-brand-gray-b bg-white p-2">
                 <div className="flex h-12 w-12 items-center justify-center rounded-full bg-brand-iridescent-blue p-2">
                   <BanknotesIcon className="aspect-square h-full text-gray-600" />
                 </div>
@@ -179,6 +179,19 @@ export default function EventDashboard() {
                 <div className="text-semibold text-sm uppercase text-gray-700">
                   Total Donated
                 </div>
+              </div>
+              <div className="flex grow flex-col items-center justify-center gap-1 rounded border border-brand-gray-b bg-white p-2">
+                <div className="flex h-12 w-12 items-center justify-center rounded-full bg-[#1da1f2] p-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="aspect-square h-full text-white"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+                  </svg>
+                </div>
+                <div className="font-extrabold">@CockroachDB</div>
               </div>
             </div>
           </div>
