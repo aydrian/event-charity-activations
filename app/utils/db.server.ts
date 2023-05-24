acf1f8aaab43d231b30590b5357c2901cccddd3c
Aydrian Howard <aydrian@gmail.com>
2023-05-24 09:06:36 -0400

    added singleton utility

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,22 +1,7 @@
 import { PrismaClient } from "@prisma/client";
+import { singleton } from "./singleton.server";
 
-let prisma: PrismaClient;
-
-declare global {
-  var __db__: PrismaClient | undefined;
-}
-
-// this is needed because in development we don't want to restart
-// the server with every change, but we want to make sure we don't
-// create a new connection to the DB with every change either.
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.__db__) {
-    global.__db__ = new PrismaClient();
-  }
-  prisma = global.__db__;
-  prisma.$connect();
-}
+const prisma = singleton("prisma", () => new PrismaClient());
+prisma.$connect();
 
 export { prisma };
