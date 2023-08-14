e412cf5f25efc5c4dee277368c1ec9c285d894f8
Aydrian Howard <aydrian@gmail.com>
2023-08-14 12:50:50 -0400

    chore: install source-map-support

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ import morgan from "morgan";
 import * as fs from "node:fs";
 import http from "node:http";
 import https from "node:https";
+import sourceMapSupport from "source-map-support";
 
 import * as build from "./build/index.js";
 
-const BUILD_PATH = "./build/index.js";
-
+sourceMapSupport.install();
 installGlobals();
+
+const BUILD_PATH = "./build/index.js";
 
 const app = express();
 
